80e3dce3d739afcb5ec8df44e9d5fc7b8087586c
annakishova <dotelady@mail.ru>
2022-10-23 21:01:46 +0400

    new authorization tests added

--- Forms/login_form.py
+++ Forms/login_form.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import NoSuchElementException
+from selenium import webdriver
 
 
 class BasePage:
@@ -10,14 +13,36 @@ class LoginFormElements:
     USERNAME = (By.XPATH, "//input[@id='user-name']")
     PASSWORD = (By.XPATH, "//input[@id='password']")
     LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR_LABEL = (By.CLASS_NAME, "error-message-container")
+    # IS_VISIBLE = expected_conditions.visibility_of_element_located
 
 
 class LoginForm(BasePage):
     element = LoginFormElements
 
-    def fill_login_form(self, username: str, password: str):
+    # @property
+    # def is_error_authorization(self) -> bool:
+    #     res = self.element.ERROR_LABEL
+
+    def check_exists_error(self):
+        try:
+            self.driver.find_element(By.CLASS_NAME, "error-message-container")
+        except NoSuchElementException:
+            return False
+        return True
+
+    def fill_login_username(self, username: str):
         self.driver.find_element(*self.element.USERNAME).send_keys(username)
+
+    def fill_login_password(self, password: str):
         self.driver.find_element(*self.element.PASSWORD).send_keys(password)
 
     def click_enter(self):
         self.driver.find_element(*self.element.LOGIN_BUTTON).click()
+
+    def clear_username_login(self):
+        self.driver.find_element(*self.element.USERNAME).clear()
+
+    def clear_password_login(self):
+        self.driver.find_element(*self.element.USERNAME).clear()
+
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,15 +1,70 @@
 from Forms.login_form import LoginForm
 from tests.constants import Sign, Url
+from selenium.webdriver.common.by import By
+import random
+import string
 
 
 class TestAuthorization:
-    def test_authorization(self, navigate_start_page):
+    def test_authorization_empty(self, navigate_start_page):
         driver = navigate_start_page
         #"Перейти к форме авторизации"
         login_form = LoginForm(navigate_start_page)
-
-        #"Заполнить параметр Логин"
-        login_form.fill_login_form(Sign.name_login, Sign.pass_login)
+        #Попытка авторизации с незаполненными полями
         login_form.click_enter()
         current_url = driver.current_url
-        assert Url.main_page_url == current_url, "основная страница"
+        assert login_form.check_exists_error(), "Не отображается ошибка авторизации "
+        assert Url.login_url == current_url, "Переход на страницу магазина без авторизации"
+
+    def test_authorization_check_valid_login(self, navigate_start_page):
+        driver = navigate_start_page
+        # "Перейти к форме авторизации"
+        login_form = LoginForm(navigate_start_page)
+        # Попытка авторизации с валидным логином и пустым паролем
+        login_form.fill_login_username(Sign.name_login)
+        login_form.click_enter()
+        current_url = driver.current_url
+        assert login_form.check_exists_error(), "Не отображается ошибка авторизации "
+        assert driver.find_element(By.XPATH, "//h3[contains(text(), 'Password is required')]").text != ""
+        assert Url.login_url == current_url, "Переход на страницу магазина без авторизации"
+
+    def test_authorization_check_valid_password(self, navigate_start_page):
+        driver = navigate_start_page
+        # "Перейти к форме авторизации"
+        login_form = LoginForm(navigate_start_page)
+        # Попытка авторизации с валидным паролем и пустым логином
+        login_form.fill_login_password(Sign.pass_login)
+        login_form.clear_username_login()
+        assert driver.find_element(By.ID, "user-name").text == ""
+        login_form.click_enter()
+        current_url = driver.current_url
+        assert login_form.check_exists_error(), "Не отображается ошибка авторизации "
+        assert driver.find_element(By.XPATH, "//h3[contains(text(), 'Username is required')]").text != ""
+        assert Url.login_url == current_url, "Переход на страницу магазина без авторизации"
+
+    def test_authorization_check_invalid_login_password(self, navigate_start_page):
+        driver = navigate_start_page
+        # "Перейти к форме авторизации"
+        login_form = LoginForm(navigate_start_page)
+        # Попытка авторизации с невалидными логином, паролем
+        s = 5
+        random_name = ''.join(random.choices(string.ascii_letters + string.digits, k=s))
+        random_pass = ''.join(random.choices(string.digits, k=s))
+        login_form.fill_login_username(random_name)
+        login_form.fill_login_password(random_pass)
+        login_form.click_enter()
+        current_url = driver.current_url
+        assert login_form.check_exists_error(), "Не отображается ошибка авторизации "
+        assert driver.find_element(By.XPATH, "//h3[contains(text(), 'do not much')]").text != ""
+        assert Url.login_url == current_url, "Переход на страницу магазина без авторизации"
+
+    def test_authorization_succesfull_authorization(self, navigate_start_page):
+        driver = navigate_start_page
+        # "Перейти к форме авторизации"
+        login_form = LoginForm(navigate_start_page)
+        #"Заполнить параметр Логин валидными значениями"
+        login_form.fill_login_username(Sign.name_login)
+        login_form.fill_login_password(Sign.pass_login)
+        login_form.click_enter()
+        current_url = driver.current_url
+        assert Url.main_page_url == current_url, "Основная страница"
